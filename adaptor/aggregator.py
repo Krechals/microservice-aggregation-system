e6c6a3a39a3de3d22530245853e2d0ad4d75df7b
Andrei Tudor TOPALĂ (101668) <topala.andrei@gmail.com>
2022-01-17 02:52:58 +0200

    dockerized adaptor

--- adaptor/aggregator.py
+++ adaptor/aggregator.py
@@ -1,10 +1,12 @@
-import paho.mqtt.client as mqtt
-import json
 from influxdb import InfluxDBClient
 from datetime import datetime, timezone
 from pytz import timezone
+import paho.mqtt.client as mqtt
+import json
+import logging
+import os
 
-db_client = InfluxDBClient(host='localhost', 
+db_client = InfluxDBClient(host='db-container', 
             port=8086,
             username='root', 
             password='pass',
@@ -25,22 +27,30 @@ def is_float(nr):
         return False
     return True
 
+# Callback from messaging broker when connecting.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("#")
 
-# The callback for when a PUBLISH message is received from the server.
+# Callback from messaging broker when someone publish on any topic.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
     host_param = msg.topic.split('/')
     location = host_param[0]
     station = host_param[1] 
     
     json_obj = json.loads(msg.payload)
-    db_elems = []
+    if os.getenv('DEBUG_DATA_FLOW'):
+        logging.info(f'Received a message by topic [{msg.topic}]')
+        if 'timestamp' in json_obj:
+            logging.info(f'Data timestamp is: {json_obj["timestamp"]}')
+        else:
+            logging.info('Data timestamp is NOW')
 
+    db_elems = []
     for key, value in json_obj.items():
         if is_int(value) or is_float(value):
+            if os.getenv('DEBUG_DATA_FLOW'):
+                logging.info(f'{msg.topic}.{key} {value}')
             db_elems.append({
                 'measurement': f'{station}.{key}',
                 'tags': {
@@ -53,7 +63,7 @@ def on_message(client, userdata, msg):
                 }
             })
 
-    # Add elements in batch.
+    # Add elements to DB in batch.
     db_client.write_points(db_elems)
 
 def broker_connect():
@@ -61,8 +71,9 @@ def broker_connect():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect("localhost")
+    client.connect("broker-container")
     client.loop_forever()
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
     broker_connect()
