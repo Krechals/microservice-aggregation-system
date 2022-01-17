72096f0ab62ccda1a1ee3cf57309424b6ef9c357
Andrei Tudor TOPALĂ (101668) <topala.andrei@gmail.com>
2022-01-17 23:26:08 +0200

    added local volume config

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ -z "${SPRC_DVP}" ]; then
+    echo 'Environment variable SPRC_DVP is not set.'
+    echo 'Example: export SPRC_DVP=/var/lib/docker/volumes'
+    exit 1
+fi
+
 docker-compose -f stack.yml build
 docker stack deploy -c stack.yml sprc3
